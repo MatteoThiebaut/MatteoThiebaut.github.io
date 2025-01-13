d9e5c2efe69ac03603a149ac1fed7b6b397af7ae
Mattéo thiebaut <lomips@hotmail.fr>
2025-01-13 20:15:31 +0100

    Try Deploy Vite Projet

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MatteoThiebaut.github.io/',
+  base: '/MatteoThiebaut.github.io',
   plugins: [vue()],
 })
