e0bc051645184e6cb15f2d6351adc8e4f913f8f3
Mattéo thiebaut <lomips@hotmail.fr>
2025-01-14 15:32:36 +0100

    deploy dist

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,13 @@
 <script setup>
 import { defineAsyncComponent } from 'vue';
 //Import Components Asynchronously
-const NavBar = defineAsyncComponent(() => import('./components/NavBar.vue'));
-const HeroSection = defineAsyncComponent(() => import('./components/HeroSection.vue'));
-const FormationSection = defineAsyncComponent(() => import('./components/FormationSection.vue'));
-const ExperiencesSection = defineAsyncComponent(() => import('./components/ExperienceSection.vue'));
-const CompetenceSection = defineAsyncComponent(() => import('./components/CompetenceSection.vue'));
-const LastestProjSection = defineAsyncComponent(() => import('./components/LatestProjSection.vue'));
-const ContactSection = defineAsyncComponent(() => import('./components/ContactSection.vue'));
-const Footer = defineAsyncComponent(() => import('./components/Footer.vue'));
+const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+const HeroSection = defineAsyncComponent(() => import('@/components/HeroSection.vue'));
+const FormationSection = defineAsyncComponent(() => import('@/components/FormationSection.vue'));
+const ExperiencesSection = defineAsyncComponent(() => import('@/components/ExperienceSection.vue'));
+const CompetenceSection = defineAsyncComponent(() => import('@/components/CompetenceSection.vue'));
+const LastestProjSection = defineAsyncComponent(() => import('@/components/LatestProjSection.vue'));
+const ContactSection = defineAsyncComponent(() => import('@/components/ContactSection.vue'));
+const Footer = defineAsyncComponent(() => import('@/components/Footer.vue'));
 
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import {fileURLToPath,URL} from 'node:url';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/MatteoThiebaut.github.io',
+
   plugins: [vue()],
+  resolve:{
+    alias:{
+      '@':fileURLToPath(new URL("./src",import.meta.url))
+    }
+  }
 })
